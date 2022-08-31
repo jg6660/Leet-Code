d6fae276c9abfd4a8473dade7a317c23d3dda804
Jaya Amit Sai <90069525+jg6660@users.noreply.github.com>
2022-08-30 19:46:00 -0500

    added perfect numbers file

---
+++ Perfect Numbers/perfect_numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num == 1)
+            return false;
+        int sum = 1;
+        for(int i=2; i*i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                System.out.println(i);
+                System.out.println(num/i);
+                sum = sum + i + num/i;
+            }
+                
+        }
+        if(sum == num)
+            return true;
+        else
+            return false;
+        
+    }
+}
