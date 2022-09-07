a4c3d56b96a7bcf2bb3e745b0e76b25386c2561b
Jaya Amit Sai <90069525+jg6660@users.noreply.github.com>
2022-09-07 15:46:52 -0500

    added majority element file

---
+++ Majority Element/majority_element.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int count = 1;
+        int Maxcount = count;
+        int num = nums[0];
+        int Maxnum = num;
+        for(int i=1; i<nums.length;i++)
+        {
+            if(nums[i]==num)
+            {
+                System.out.println("Hi");
+                count++;
+                num = nums[i];
+                if(i==nums.length-1 && Maxcount<=count)
+                {
+                    Maxcount = count;
+                    Maxnum = num;
+                }
+            }
+            else
+            {
+                if(count>Maxcount)
+                {
+                    System.out.println("hello");
+                    Maxcount = count;
+                    Maxnum = num;
+                    count =1;
+                    num = nums[i];
+                }
+                else
+                {
+                    System.out.println("Bye");
+                    count =1;
+                    num = nums[i];
+                }
+            }
+        }
+        return Maxnum;
+        
+    }
+}
