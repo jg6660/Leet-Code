c804840d9140363ed478807121ef9b69b75b0a2f
Jaya Amit Sai <90069525+jg6660@users.noreply.github.com>
2022-09-16 11:10:16 -0500

    added linked list cycle java file

---
+++ LinkedList/Linked List Cycle/linkedlistcycle.java
@@ -0,0 +1,34 @@
+/* Floyd's cycle finding algorithm */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+
+/* Hash Table */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            }
+            nodesSeen.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
