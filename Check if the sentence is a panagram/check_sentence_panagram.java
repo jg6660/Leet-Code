fbc7e22dba37ceab6a6ac67ee9bf2669400c2392
Jaya Amit Sai <90069525+jg6660@users.noreply.github.com>
2022-10-04 16:21:19 -0500

    added extra set solution as well

--- Check if the sentence is a panagram/check_sentence_panagram.java
+++ Check if the sentence is a panagram/check_sentence_panagram.java
@@ -18,3 +18,20 @@ class Solution {
             return true;
     }
 }
+
+
+/* Leet code Set solution */
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        // Empty set 'seen'.
+        Set<Character> seen = new HashSet<>();
+        
+        // Iterate over 'sentence' add every letter to 'seen'.
+        for (char currChar : sentence.toCharArray())
+            seen.add(currChar);
+        
+        // If the size of 'seen' is 26, then 'sentence' is a pangram.
+        return seen.size() == 26;
+    }
+}
