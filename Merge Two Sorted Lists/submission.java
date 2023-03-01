5a706aeafae90b23ee1643384caee8cd9293856f
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2023-03-01 13:21:06 -0600

    added my submission file

---
+++ Merge Two Sorted Lists/submission.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1 == null) {return list2;}
+        if(list2 == null) {return list1;}
+
+        ListNode head;
+
+        if(list1.val < list2.val){
+            head = list1;
+            list1 = list1.next;
+        }
+        else{
+            head = list2;
+            list2 = list2.next;
+        }
+
+        head.next = mergeTwoLists(list1,list2);
+
+        return head;
+
+
+        
+
+    }
+}
