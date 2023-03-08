6c7711caf46802087216daf3c4764934b4d5f7e5
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2023-03-08 10:54:08 -0600

    modified the submission file

--- Count Primes/self_submission.java
+++ Count Primes/self_submission.java
@@ -1,21 +1,31 @@
 class Solution {
     public boolean prime(int n){
         if (n <= 1)
-		    return false;
-            
-		for (int i = 2; i < n; i++)
-			if (n % i == 0)
-				return false;
-
-		return true;
+            return false;
+ 
+        // Check if number is 2
+        else if (n == 2)
+            return true;
+ 
+        // Check if n is a multiple of 2
+        else if (n % 2 == 0)
+            return false;
+ 
+        // If not, then just check the odds
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
 
     }
     public int countPrimes(int n) {
         int count =0;
         for(int i = 0;i<n;i++)
         {
-            if(prime(i))
+            if(prime(i)){
                 count++;
+            }
         }
         return count;
     }
