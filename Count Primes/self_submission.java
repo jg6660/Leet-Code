cb065ca8604d31147aff4d739a72f235f3685122
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2023-03-08 11:19:37 -0600

    previous submission will exceed the time limit

--- Count Primes/self_submission.java
+++ Count Primes/self_submission.java
@@ -1,32 +1,26 @@
 class Solution {
-    public boolean prime(int n){
-        if (n <= 1)
-            return false;
- 
-        // Check if number is 2
-        else if (n == 2)
-            return true;
- 
-        // Check if n is a multiple of 2
-        else if (n % 2 == 0)
-            return false;
- 
-        // If not, then just check the odds
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            if (n % i == 0)
-                return false;
-        }
-        return true;
-
-    }
     public int countPrimes(int n) {
-        int count =0;
-        for(int i = 0;i<n;i++)
-        {
-            if(prime(i)){
-                count++;
-            }
-        }
-        return count;
+       if(n<=2){
+           return 0;
+       }
+
+       boolean[] result = new boolean[n];
+
+       for(int i=2; i<= (int)Math.sqrt(n); i++){
+           if(result[i] == false){
+               for(int j= i*i; j<n; j+=i){
+                    result[j] = true;
+               }
+           }
+       }
+
+       int countprimes = 0;
+       for(int i=2; i<n;i++){
+           if(result[i] == false){
+               countprimes++;
+           } 
+       }
+
+       return countprimes;
     }
 }
