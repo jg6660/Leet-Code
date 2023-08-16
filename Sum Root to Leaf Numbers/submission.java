cf5760f5238a70e414f5447bd543dc956f53f265
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2023-08-15 23:18:04 -0700

    Create submission.java

---
+++ Sum Root to Leaf Numbers/submission.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<Integer>();
+
+    public int sumNumbers(TreeNode root) {
+        int value = 0;
+        addNumbers(root, value);   
+
+        int sum = 0;
+        for(int val : result){
+            sum += val;
+        } 
+
+        return sum;
+    }
+
+    private void addNumbers(TreeNode node, int value){
+        if(node != null){
+        value = value*10 + node.val;
+        if(node.left == null && node.right == null){
+            result.add(value);
+        }
+        addNumbers(node.left, value);
+        addNumbers(node.right, value);
+        }
+    }
+}
