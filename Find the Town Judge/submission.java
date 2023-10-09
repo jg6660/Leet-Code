9f1c0efed20863095b12cf304ba6e1e6414a65eb
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2023-10-09 14:06:46 -0700

    Create submission.java

---
+++ Find the Town Judge/submission.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+     if(trust.length < n-1){
+         return -1;
+     }
+
+     int[] indegrees = new int[n+1];
+     int[] outdegrees = new int[n+1];
+
+     for(int[] relation: trust){
+         outdegrees[relation[0]]++;
+         indegrees[relation[1]]++;
+     }
+
+     for(int i=1; i<n+1; i++){
+         if(indegrees[i] == n-1 && outdegrees[i] == 0){
+             return i;
+         }
+     }
+
+     return -1;
+    }
+}
