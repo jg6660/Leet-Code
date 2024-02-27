24120c0b193bc42b7007d82b7361cd6a2c4906b7
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2024-02-26 19:28:14 -0800

    Create submission.java

---
+++ Meeting Rooms 2/submission.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(a,b));
+
+        pq.add(intervals[0][1]);
+
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0] >= pq.peek()){
+                pq.poll();
+            }
+
+            pq.add(intervals[i][1]);
+        }
+
+        return pq.size();
+    }
+}
