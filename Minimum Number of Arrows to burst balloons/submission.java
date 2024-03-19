b6a5fa9bb06cd5fb7080a71f124bf17bfbf8f1bf
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2024-03-19 15:41:10 -0700

    Create submission.java

---
+++ Minimum Number of Arrows to burst balloons/submission.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a,b) -> Integer.compare(a[1],b[1]));
+        int count = 1;
+
+        int[] point = points[0];
+        long minPoint = point[1];
+        for(int i=1; i<points.length; i++){
+            if(minPoint>=points[i][0]){continue;}
+            else{
+                minPoint = points[i][1];
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
