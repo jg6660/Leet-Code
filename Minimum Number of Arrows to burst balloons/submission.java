a7c69b8fde85f28ab78f0d4a503a94c7c26732a8
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2024-03-19 15:43:08 -0700

    Update submission.java

--- Minimum Number of Arrows to burst balloons/submission.java
+++ Minimum Number of Arrows to burst balloons/submission.java
@@ -6,8 +6,7 @@ class Solution {
         int[] point = points[0];
         long minPoint = point[1];
         for(int i=1; i<points.length; i++){
-            if(minPoint>=points[i][0]){continue;}
-            else{
+            if(minPoint < points[i][0]){
                 minPoint = points[i][1];
                 count++;
             }
