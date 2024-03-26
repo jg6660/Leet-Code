fdb79a50e063ff462c46ded1832f3d6cb4c3d14f
Jaya Amit Sai Gurrala <jg6660@nyu.edu>
2024-03-26 11:27:34 -0700

    Create another_solution.java

---
+++ First Missing Positive/another_solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        return i+1;
+    }
+    
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
+
+//https://leetcode.com/problems/first-missing-positive/solutions/17083/o-1-space-java-solution/?envType=daily-question&envId=2024-03-26
